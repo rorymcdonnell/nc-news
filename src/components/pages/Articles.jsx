a1692e0f4802e78456ab14366d7f803a173d002b
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-07-14 12:17:42 +0200

    fixed all articles page so now it shows all articles

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -21,19 +21,46 @@ const Articles = () => {
       </div>
     );
 
+  if (
+    topic_slug !== "cooking" &&
+    topic_slug !== "coding" &&
+    topic_slug !== "football"
+  ) {
+    return (
+      <div className="articles-section">
+        <ul>
+          {articles.map(
+            ({
+              article_id,
+              title,
+              body,
+              topic,
+              author,
+              number_of_comments,
+            }) => {
+              return (
+                <li key={article_id}>
+                  <h2>{title}</h2>
+                  <h3>{topic}</h3>
+                  <p>{body}</p>
+                  <p>{author}</p>
+                  <p>{number_of_comments}</p>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className="articles-section">
-      <h1>All Articles:</h1>
-      {console.log(topic_slug)}
       <ul>
         {articles
           .filter((article) => {
             if (article.topic === undefined) {
-              return (
-                article.topic === "coding" ||
-                article.topic === "cooking" ||
-                article.topic === "football"
-              );
+              return article;
             } else {
               return article.topic === topic_slug;
             }
